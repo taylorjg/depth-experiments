92839918c2df2018507a46cb58fe4199ef8e2801
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-06-04 13:41:13 +0100

    Play around with gl_FragCoord in fragment shader

--- src/my-fragment-shader.glsl
+++ src/my-fragment-shader.glsl
@@ -1,5 +1,15 @@
 uniform vec3 color;
 
 void main() {
-  gl_FragColor = vec4(color, 1.0);
+  float r = color.r;
+  float g = color.g;
+  float b = color.b;
+  gl_FragColor = vec4(r, g, b, 1.0);
+
+  // float v1 = gl_FragCoord.w;
+  // float v = v1 > 0.12 ? 0.8 : 0.2;
+  // float v = gl_FragCoord.w;
+
+  // float v = gl_FragCoord.w;
+  // gl_FragColor = vec4(v, v, v, 1.0);
 }
