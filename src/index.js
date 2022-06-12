f39486eb16a5d612efad33c5b95fc9c0fdad7441
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2022-06-12 13:24:56 +0100

    Checkpointing 3

--- src/index.js
+++ src/index.js
@@ -47,14 +47,12 @@ const main = async () => {
   const h = WINDOW_SIZE
 
   const canvas1 = document.getElementById('canvas1')
-  const renderer1 = new THREE.WebGLRenderer({ antialias: true, canvas: canvas1 })
-  renderer1.setPixelRatio(window.devicePixelRatio)
-  renderer1.setSize(w, h)
-
   const canvas2 = document.getElementById('canvas2')
-  const renderer2 = new THREE.WebGLRenderer({ antialias: true, canvas: canvas2 })
-  renderer2.setPixelRatio(window.devicePixelRatio)
-  renderer2.setSize(w, h)
+  const canvas3 = document.getElementById('canvas3')
+
+  const renderer = new THREE.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize(w, h)
 
   const mainScene = new THREE.Scene()
   const mainCamera = new THREE.PerspectiveCamera(45, w / h, 0.1, 50)
@@ -67,28 +65,54 @@ const main = async () => {
   const hp = h * window.devicePixelRatio
 
   const renderTarget = new THREE.WebGLRenderTarget(wp, hp)
+  renderTarget.type = THREE.FloatType
   renderTarget.depthTexture = new THREE.DepthTexture(wp, hp)
 
-  const postCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
-  const postScene = new THREE.Scene()
-  postScene.add(postCamera)
-
-  const map = renderTarget.depthTexture
-  // const map = renderTarget.texture
-
-  const quadGemoetry = new THREE.PlaneBufferGeometry(2, 2)
-  const quadMaterial = new THREE.MeshBasicMaterial({ map })
-  const quadMesh = new THREE.Mesh(quadGemoetry, quadMaterial)
-  postScene.add(quadMesh)
-
-  const myRender = () => {
-    renderer1.setRenderTarget(renderTarget)
-    renderer1.render(mainScene, mainCamera)
-    renderer1.setRenderTarget(null)
-    renderer1.render(postScene, postCamera)
+  const makePostScene = (texture) => {
+    const postScene = new THREE.Scene()
+    const quadGemoetry = new THREE.PlaneBufferGeometry(2, 2)
+    const quadMaterial = new THREE.MeshBasicMaterial({ map: texture })
+    const quadMesh = new THREE.Mesh(quadGemoetry, quadMaterial)
+    postScene.add(quadMesh)
+    return postScene
   }
 
-  myRender()
+  const postScene1 = makePostScene(renderTarget.depthTexture)
+  const postScene2 = makePostScene(renderTarget.texture)
+
+  const setCanvasSize = canvas => {
+    canvas.style.width = `${w}px`
+    canvas.style.height = `${h}px`
+    canvas.width = wp
+    canvas.height = hp
+  }
+
+  setCanvasSize(canvas1)
+  setCanvasSize(canvas2)
+  setCanvasSize(canvas3)
+
+  const canvasContext1 = canvas1.getContext('2d')
+  const canvasContext2 = canvas2.getContext('2d')
+  const canvasContext3 = canvas3.getContext('2d')
+
+  const postCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+
+  const render = () => {
+    renderer.setRenderTarget(renderTarget)
+    renderer.render(mainScene, mainCamera)
+
+    renderer.setRenderTarget(null)
+    renderer.render(mainScene, mainCamera)
+    canvasContext1.drawImage(renderer.domElement, 0, 0)
+
+    renderer.render(postScene1, postCamera)
+    canvasContext2.drawImage(renderer.domElement, 0, 0)
+
+    renderer.render(postScene2, postCamera)
+    canvasContext3.drawImage(renderer.domElement, 0, 0)
+  }
+
+  render()
 }
 
 main()
